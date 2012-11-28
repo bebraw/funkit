5185bb0776a10f9f626d0f1b2339ca6d0a7e96af
Juho Vepsäläinen <bebraw@gmail.com>
2012-11-28 15:32:19 +0200

    Test array#chunk using an invariant

--- tests/array/chunk.js
+++ tests/array/chunk.js
@@ -1,6 +1,8 @@
 var suite = require('suite.js');
 var partial = require('funkit/functional/partial');
+var g = require('generators.js');
 var f = require('funkit/array');
+var equals = require('funkit/operators/equals');
 
 suite(partial(f.chunk, 2), [
     [[]], [],
@@ -8,3 +10,10 @@ suite(partial(f.chunk, 2), [
     [[1, 2, 3]], [[1, 2], [3]],
     [['a', 2, 'c']], [['a', 2], ['c']]
 ]);
+
+suite(f.chunk, suite.generate(1000,
+    [g.number(1000), g.array(10, g.character)],
+    function(op, len, arr) {
+        return equals(f.concat(op(len, arr)), arr);
+    })
+);
