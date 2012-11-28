e48111f44692317d491ac8d065ba0b8bac486864
Juho Vepsäläinen <bebraw@gmail.com>
2012-11-28 16:11:53 +0200

    Test that err raises exception properly

--- tests/common/err.js
+++ tests/common/err.js
@@ -1,4 +1,12 @@
 var suite = require('suite.js');
+var partial = require('funkit/functional/partial');
+var noop = require('funkit/functional/noop');
 var f = require('funkit/common');
 
-// TODO
+var AssertionError = require('assert').AssertionError;
+
+var fn = f.err(noop);
+
+suite(partial(fn, AssertionError), [
+    'foo', suite.exception(AssertionError)
+]);
