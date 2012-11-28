e01fa74296e840060d4cd8fbcc68a5a565c80add
Juho Vepsäläinen <bebraw@gmail.com>
2012-11-28 16:16:44 +0200

    Define invariant for functional#partial

--- tests/functional/partial.js
+++ tests/functional/partial.js
@@ -1,4 +1,11 @@
 var suite = require('suite.js');
+var g = require('generators.js');
 var f = require('funkit/functional');
+var sum = require('funkit/operators/sum');
 
-// TODO
+suite(sum, suite.generate(1000,
+    [g.number(1000), g.number(1000)],
+    function(op, a, b) {
+        return op(a, b) == f.partial(op, a)(b);
+    })
+);
