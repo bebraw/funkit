de12589809e01f40ed719bd5360e75478dc637a8
Juho Vepsäläinen <bebraw@gmail.com>
2012-12-06 14:44:50 +0200

    Make functional#chain test more robust

--- tests/functional/chain.js
+++ tests/functional/chain.js
@@ -3,17 +3,11 @@ var ops = require('funkit/ops');
 var f = require('funkit/functional');
 
 suite(checkChain, [
-    [[1, 5, 1]], 7
+    [[1, 5, 1]], 10
 ]);
 
 function checkChain(a) {
     var _ = f.chain(f.map, f.filter, f.reduce);
 
-    return _(a).reduce(ops.add).val();
-
-    // XXX: problematic since map passes i to mul leading to val * i!
-    // XXX: same problem with add!
-    //var r = _(a).map(f.partial(ops.mul, 2));
-    //r = r.filter(f.partial(ops.gt, 2))
-    //return r.reduce(ops.add).val();
+    return _(a).map(f.partial(ops.mul, 2)).filter(f.partial(ops.gt, 2)).reduce(ops.add).val();
 }
