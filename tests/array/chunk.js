b492fb659f17d803251e8cc8b89272acc98d2c14
Juho Vepsäläinen <bebraw@gmail.com>
2012-12-06 21:28:43 +0200

    Remove redundant unit tests
    Those are covered by invariants now.

--- tests/array/chunk.js
+++ tests/array/chunk.js
@@ -1,19 +1,10 @@
 var suite = require('suite.js');
 var g = require('generators.js');
-var partial = require('funkit/functional/partial');
 var f = require('funkit/array');
 var equals = require('funkit/ops/equals');
-var chunk = require('funkit/array/chunk');
 
 suite._amount = 1000;
 suite._generator = g.any;
-
-suite(partial(f.chunk, 2), [
-    [[]], [],
-    [[1]], [[1]],
-    [[1, 2, 3]], [[1, 2], [3]],
-    [['a', 2, 'c']], [['a', 2], ['c']]
-]);
 
 suite(f.chunk, function(op, len, arr) {
     return equals(f.concat(op(len, arr)), arr);
--- tests/array/concat.js
+++ tests/array/concat.js
@@ -3,11 +3,8 @@ var g = require('generators.js');
 var f = require('funkit/array');
 var equals = require('funkit/ops/equals');
 
-suite(f.concat, [
-    [[1]], [1],
-    [[[1]]], [1],
-    [[['a', 'b', 'c'], [1, 2, 3]]], ['a', 'b', 'c', 1, 2, 3]
-]);
+suite._amount = 1000;
+suite._generator = g.any;
 
 suite(f.concat, function(op, arr) {
     return equals(f.concat(f.chunk(3, arr)), arr);
