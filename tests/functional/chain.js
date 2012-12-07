382c3cf39ff912f5f98ce1991e2702f5fc72d6d6
Juho Vepsäläinen <bebraw@gmail.com>
2012-12-07 17:52:52 +0200

    Make functional#chain behave properly when no argument is provided
    In this case it will just use the internal value.

--- tests/functional/chain.js
+++ tests/functional/chain.js
@@ -1,6 +1,7 @@
 var suite = require('suite.js');
 var ops = require('funkit/ops');
 var f = require('funkit/functional');
+var capitalize = require('funkit/string/capitalize');
 
 suite(checkChain, [
     [[1, 5, 1]], 10
@@ -24,4 +25,14 @@ function checkArrayChain(a) {
 
 function op(a, _) {
     return _(a).map(f.partial(ops.mul, 2)).filter(f.partial(ops.gt, 2)).reduce(ops.add).val();
+}
+
+suite(checkCapitalize, [
+    'foobar', 'Foobar'
+]);
+
+function checkCapitalize(a) {
+    var _ = f.chain(capitalize);
+
+    return _(a).capitalize().val();
 }
