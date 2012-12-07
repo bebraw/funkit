3c3ba4025d0d4cb35f98dde2af82469bebc6628a
Juho Vepsäläinen <bebraw@gmail.com>
2012-12-07 17:43:59 +0200

    Make main modules chaining by default
    Now it is possible to perform something like this:
    
    ```javascript
    var f = require('funkit');
    
    f.functional([1, 3, 4]).map(f.functional.partial(f.ops.mul, 2)).val();
    ```
    
    There are still some glitches around. Esp. some string ops seem broken
    using this scheme. Might be a bug in functional#chain.

--- tests/functional/chain.js
+++ tests/functional/chain.js
@@ -9,5 +9,19 @@ suite(checkChain, [
 function checkChain(a) {
     var _ = f.chain(f.map, f.filter, f.reduce);
 
+    return op(a, _);
+}
+
+suite(checkArrayChain, [
+    [[1, 5, 1]], 10
+]);
+
+function checkArrayChain(a) {
+    var _ = f.chain([f.map, f.filter, f.reduce]);
+
+    return op(a, _);
+}
+
+function op(a, _) {
     return _(a).map(f.partial(ops.mul, 2)).filter(f.partial(ops.gt, 2)).reduce(ops.add).val();
 }
