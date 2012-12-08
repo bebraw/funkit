d66c2777b9413afd8b2fcfedde324c6ad267550f
Juho Vepsäläinen <bebraw@gmail.com>
2012-12-08 19:10:15 +0200

    Fix math#randint test and implementation and array#chunk

--- tests/math/randint.js
+++ tests/math/randint.js
@@ -5,7 +5,8 @@ var f = require('funkit/math');
 suite._amount = 1000;
 suite._generator = g.any;
 
-// XXX: figure out why a and b are NaN always (improve any?)
 suite(f.randint, function(op, a, b) {
-    return !a || !b || f.between(a, b, op(a, b));
+    var res = op(a, b);
+
+    return !res || f.between(a, b, res);
 });
