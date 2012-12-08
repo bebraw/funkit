2efe843df6d1bbcce94a2545617de2eaf7cbe421
Juho Vepsäläinen <bebraw@gmail.com>
2012-12-08 16:14:39 +0200

    Make functional#partial retain type data

--- tests/functional/partial.js
+++ tests/functional/partial.js
@@ -1,7 +1,9 @@
+var is = require('is-js');
 var suite = require('suite.js');
 var g = require('generators.js');
 var f = require('funkit');
 var add = f.ops.add;
+var gt = f.ops.gt;
 
 suite(add, suite.generate(1000,
     [g.number(1000), g.number(1000)],
@@ -9,3 +11,15 @@ suite(add, suite.generate(1000,
         return op(a, b) == f.partial(op, a)(b);
     })
 );
+
+var gtZero = f.partial(gt, 0);
+
+suite(f.partial(getProperty, gtZero), [
+    '_name', gt._name,
+    '_doc', gt._doc,
+    '_invariants', [[is.number], [is.fn]]
+]);
+
+function getProperty(fn, k) {
+    return fn[k];
+}
