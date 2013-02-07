a09dd149cec39a723552ba31c48e0126a97664e7
Juho Vepsalainen <bebraw@gmail.com>
2013-02-07 16:03:22 +0200

    Add index to parallel
    Closes #3.

--- tests/async/parallel.js
+++ tests/async/parallel.js
@@ -1,10 +1,13 @@
+var assert = require('assert');
 var suite = require('suite.js');
 var f = require('funkit');
+var is = require('is-js');
 
 suite(suite.async(f.partial(f.async.parallel, getName)), [
     [['1', '2', '3']], ['1', '2', '3']
 ]);
 
-function getName(name, done) {
+function getName(name, done, i) {
+    assert(is.number(i));
     done(null, name);
 }
