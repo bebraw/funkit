ae39a891ac91f5b800d61798ddf972caa5404337
Juho Vepsalainen <bebraw@gmail.com>
2013-02-09 12:24:06 +0200

    Implement math#atan

---
+++ tests/math/atan.js
@@ -0,0 +1,12 @@
+var suite = require('suite.js');
+var arr = require('funkit/array');
+var f = require('funkit/math');
+
+/*
+XXX: enable once suite.js can generate tests more effectively
+suite(f.atan, function(op, a) {
+    var res = op(a);
+
+    return -Math.PI / 2 <= res && res <= Math.PI / 2;
+});
+*/
