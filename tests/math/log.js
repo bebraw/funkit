8a66672817a36612d7e9e5a242f9f9c0f2439d83
Juho Vepsalainen <bebraw@gmail.com>
2013-02-09 14:35:58 +0200

    Make math#log test not fail with zero

--- tests/math/log.js
+++ tests/math/log.js
@@ -2,5 +2,5 @@ var suite = require('suite.js');
 var f = require('funkit/math');
 
 suite(f.log, function(op, a) {
-    return f.log(f.pow(Math.E, a));
+    return a === 0 || f.log(f.pow(Math.E, a));
 });
