9374c3b8cb18669b7b4184a8d005a8e1bf22e207
Juho Vepsalainen <bebraw@gmail.com>
2013-02-09 12:12:12 +0200

    Implement math#asin

---
+++ tests/math/asin.js
@@ -0,0 +1,10 @@
+var suite = require('suite.js');
+var arr = require('funkit/array');
+var f = require('funkit/math');
+
+/*
+XXX: enable once suite.js can generate tests more effectively
+suite(f.asin, function(op, a) {
+    return -Math.PI / 2 <= op(a) <= Math.PI / 2;
+});
+*/
