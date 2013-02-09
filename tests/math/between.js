0c1991bdf777cf78f407d463bf0191d8636d1e38
Juho Vepsalainen <bebraw@gmail.com>
2013-02-09 12:44:54 +0200

    Make math#between consistent with math#clamp

--- tests/math/between.js
+++ tests/math/between.js
@@ -2,6 +2,6 @@ var suite = require('suite.js');
 var arr = require('funkit/array');
 var f = require('funkit/math');
 
-suite(f.between, function(op, a, b, val) {
-    return op(a, b, val) == (arr.index(val, f.range(a, b + 1)) >= 0);
+suite(f.between, function(op, val, a, b) {
+    return op(val, a, b) == (arr.index(val, f.range(a, b + 1)) >= 0);
 });
--- tests/math/randint.js
+++ tests/math/randint.js
@@ -8,5 +8,5 @@ suite._generator = g.any;
 suite(f.randint, function(op, a, b) {
     var res = op(a, b);
 
-    return !res || f.between(a, b, res);
+    return !res || f.between(res, a, b);
 });
