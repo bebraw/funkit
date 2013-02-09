3ba31be88a41c82ea58b60efc559eaaafba4daa4
Juho Vepsalainen <bebraw@gmail.com>
2013-02-09 12:10:26 +0200

    Implement math#acos

---
+++ tests/math/acos.js
@@ -0,0 +1,10 @@
+var suite = require('suite.js');
+var arr = require('funkit/array');
+var f = require('funkit/math');
+
+/*
+XXX: enable once suite.js can generate tests more effectively
+suite(f.acos, function(op, a) {
+    return 0 <= op(a) <= Math.PI;
+});
+*/
